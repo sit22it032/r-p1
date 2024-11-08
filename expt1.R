2da89120f339034409caf0063e2edac99b1e647f
Rajarajan <166971048+sit22it032@users.noreply.github.com>
2024-11-08 11:56:54 -0500

    expt1

---
+++ expt1.R
@@ -0,0 +1,11 @@
+#Variables and Data Types
+x <- 10    # Integer
+y <- 5.5   # Numeric (float)
+z <- "Hello"   # Character
+is_true <- TRUE  # Logical (boolean)
+
+#checking the data type
+class(x)    # Returns "numeric" for 10
+typeof(y)   # Returns "double" for 5.5
+
+
